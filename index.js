aa367340270b28c4edf478877af44dac6c259418
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-06 15:37:39 -0500

    copmlete lap

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const cards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
